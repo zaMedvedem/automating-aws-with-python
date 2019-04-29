640e5ac0736f5bbb70b471a546d05e6df20fa8a6
Yevgen Reus <yevgenreus@zaMedvedem.local>
2019-04-29 00:09:11 -0500

    Adding scale-down function to the project

---
+++ 02-notifon/scale-down.py
@@ -0,0 +1,6 @@
+import boto3
+session = boto3.Session(profile_name='pythonAutomation')
+as_client = session.client('autoscaling')
+as_client.describe_auto_scaling_groups()
+as_client.describe_policies()
+as_client.execute_policy(AutoScalingGroupName='Notifon Example Group', PolicyName='Scale Down')
