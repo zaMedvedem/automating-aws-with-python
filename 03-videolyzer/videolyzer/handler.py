1e9343931927964b9eb98e91e446b58a7490e05f
Yevgen Reus <yevgenreus@zaMedvedem.local>
2019-04-29 22:06:57 -0500

    Save data from get_label_detection to dynamodb

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,7 +1,8 @@
-import urllib
-import boto3
-import os
 import json
+import os
+import urllib
+
+import boto3
 
 def start_label_detection(bucket, key):
     rekognition_client = boto3.client('rekognition')
@@ -12,13 +13,10 @@ def start_label_detection(bucket, key):
                 'Name': key
             }
         },
-
         NotificationChannel={
-            'SNSTopicArn': os. environ['REKOGNITION_SNS_TOPIC_ARN'],
-            'RoleArn': os. environ['REKOGNITION_ROLE_ARN']
+            'SNSTopicArn': os.environ['REKOGNITION_SNS_TOPIC_ARN'],
+            'RoleArn': os.environ['REKOGNITION_ROLE_ARN']
         })
-
-    print(response)
 
     return
 
@@ -41,8 +39,35 @@ def get_video_labels(job_id):
 
     return response
 
+def make_item(data):
+    if isinstance(data, dict):
+        return { k: make_item(v) for k, v in data.items() }
+
+    if isinstance(data, list):
+        return [ make_item(v) for v in data ]
+
+    if isinstance(data, float):
+        return str(data)
+
+    return data
+
 def put_labels_in_db(data, video_name, video_bucket):
-    pass
+    del data['ResponseMetadata']
+    del data['JobStatus']
+
+    data['videoName'] = video_name
+    data['videoBucket'] = video_bucket
+
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ['DYNAMODB_TABLE_NAME']
+    videos_table = dynamodb.Table(table_name)
+
+    data = make_item(data)
+
+    videos_table.put_item(Item=data)
+
+    return
+
 # Lambda events
 
 def start_processing_video(event, context):
@@ -51,6 +76,7 @@ def start_processing_video(event, context):
             record['s3']['bucket']['name'],
             urllib.parse.unquote_plus(record['s3']['object']['key'])
         )
+
 
     return
 
@@ -62,7 +88,6 @@ def handle_label_detection(event, context):
         s3_bucket = message['Video']['S3Bucket']
 
         response = get_video_labels(job_id)
-        print(response)
         put_labels_in_db(response, s3_object, s3_bucket)
 
     return
